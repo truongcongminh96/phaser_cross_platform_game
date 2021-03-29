b45be6acb16505acbf93c3748bd8c7429cda6604
Minh Truong <truongcongminh96@gmail.com>
2021-03-29 10:25:15 +0700

    Add map to the game

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Phaser from 'phaser';
 
 import PlayScene from './scenes/Play';
+import PreloadScene from './scenes/Preload';
 
 const WIDTH = 1280;
 const HEIGHT = 600;
@@ -10,7 +11,7 @@ const SHARED_CONFIG = {
     height: HEIGHT
 };
 
-const Scenes = [PlayScene];
+const Scenes = [PreloadScene, PlayScene];
 const createScene = Scene => new Scene(SHARED_CONFIG);
 const initScenes = () => Scenes.map(createScene);
 
