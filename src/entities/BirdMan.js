a93cca6757e8edc147318cac6d4e6b3015707ee9
Minh Truong <truongcongminh96@gmail.com>
2021-04-12 10:01:46 +0700

    refactor, add birdman animations

--- src/entities/BirdMan.js
+++ src/entities/BirdMan.js
@@ -1,8 +1,15 @@
 import Enemy from './Enemy';
+import iniAnims from './anims/birdmanAnims';
 
 class Birdman extends Enemy {
     constructor(scene, x, y) {
         super(scene, x, y, 'birdman');
+        iniAnims(scene.anims);
+    }
+
+    update(time, delta) {
+        super.update(time, delta);
+        this.play('birdman-idle', true)
     }
 }
 
